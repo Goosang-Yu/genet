5d97fd45abfac3deed68c805094eb65ed75a656c
Goosang Yu <gsyu93@gmail.com>
2022-08-21 18:19:59 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
