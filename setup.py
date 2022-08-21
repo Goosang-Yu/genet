8967b3f3e3be800f1653e5bca63d2aceb64171fa
Goosang Yu <gsyu93@gmail.com>
2022-08-21 18:17:19 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
     
 setuptools.setup(
-    name="GenET",
+    name="genet",
     version="0.0.1",
     author="Goosang Yu",
     author_email="gsyu93@gmail.com",
