d9699621aa3fdec004e129453717a05940d1f94b
Goosang Yu <gsyu93@gmail.com>
2022-09-03 01:11:10 +0900

    update setuptools

--- setup.py
+++ setup.py
@@ -7,25 +7,24 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 VERSION = genet.__version__
 
 setuptools.setup(
-    name="genet",
-    version=VERSION,
-    author="Goosang Yu",
-    author_email="gsyu93@gmail.com",
-    description="Genome Editing Toolkit",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Goosang-Yu/genet",
-    install_requires = [
-        'pandas'
-    ],
-    project_urls={
-        "Bug Tracker": "https://github.com/Goosang-Yu/genet/issues",
-    },
+    name            = "genet",
+    version         = VERSION,
+    author          = "Goosang Yu",
+    author_email    = "gsyu93@gmail.com",
+    description     = "Genome Editing Toolkit",
+    url             = "https://github.com/Goosang-Yu/genet",
+    packages        = setuptools.find_packages(exclude = ['dev_ing', 'dev_ing.*']),
+    python_requires = ">=3.6",
+    
+    install_requires = ['pandas'],
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    project_urls={"Bug Tracker": "https://github.com/Goosang-Yu/genet/issues"},
+    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
     include_package_data=True,
 )
