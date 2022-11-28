dec834fe2a7273a2badddefb648af611efb949be
Goosang Yu <gsyu93@gmail.com>
2022-11-28 19:21:29 +0900

    update setup

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setuptools.setup(
         'torchaudio==0.11.0',
         'protobuf==3.20.*',
         'silence_tensorflow',
-        'genet_models @ get+https://github.com/Goosang-Yu/genet-models.git',
+        'genet_models@git+https://github.com/Goosang-Yu/genet-models.git',
         ],
 
     dependency_links=[
