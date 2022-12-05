0412f5e941be48760678469d7b3e0dc2ab2d60fe
Goosang Yu <gsyu93@gmail.com>
2022-12-05 19:42:54 +0900

    update GetGene modules

--- genet/database/functional.py
+++ genet/database/functional.py
@@ -3,42 +3,40 @@ import genet.utils
 from Bio import Entrez, GenBank, SeqIO
 
 class GetGene:
+    '''
+    NCBI에서 reference gene을 찾기 위한 함수.
+    기본적으로 biopython의 Entrez module과 GenBank module을 사용한다. 
+
+    default는 human genome에서 reference gene sequence를 가져오는 것이다. \n
+    하지만 만약 사용자가 다른 sequence 정보를 가져오고 싶다면,\n
+    search_option에서 원하는 조건식을 넣어주면 되고,
+    species도 다른 것으로 바꿔줄 수 있다.  
+
+    하지만 species를 적어줄 때에는 정확한 학명으로 적어줘야 한다.\n
+    Human (X) / Homo sapiens (O) > 확인 필요, human도 될 듯?\n
+    Mouse (X) / Mus musculus (O)
+
+    만약 Mus musculus라고 전체로 적어주지 않고 Mus라고만 적으면,\n
+    정확한 RefSeq이 찾아지지 않는다. \n
+    학명에 Mus를 포함하는 것이 총 3종류가 있기 때문이다.\n
+    Mus caroli / Mus musculus / Mus pahari
+    
+    ## example:
+    >>> from genet import database as db
+
+    >>> # To get BRCA1 gene sequence information from mouse
+    >>> gene = db.GetGene('BRCA1', species='Mus musculus')
+
+    '''
     def __init__(self, 
                  gene_name:str,
                  species:str = 'Homo sapiens',
                  search_option:str = 'AND biomol_genomic[PROP] AND RefSeqGene[Filter]',
                  ):
 
-        '''
-        NCBI에서 reference gene을 찾기 위한 function.\n
-        기본적으로 biopython의 Entrez module과 GenBank module을 사용한다. 
-
-        default는 human genome에서 reference gene sequence를 가져오는 것이다. \n
-        하지만 만약 사용자가 다른 sequence 정보를 가져오고 싶다면,\n
-        search_option에서 원하는 조건식을 넣어주면 되고,
-        species도 다른 것으로 바꿔줄 수 있다.  
-
-        하지만 species를 적어줄 때에는 정확한 학명으로 적어줘야 한다.\n
-        Human (X) / Homo sapiens (O) > 확인 필요, human도 될 듯?\n
-        Mouse (X) / Mus musculus (O)
-
-        만약 Mus musculus라고 전체로 적어주지 않고 Mus라고만 적으면,\n
-        정확한 RefSeq이 찾아지지 않는다. \n
-        학명에 Mus를 포함하는 것이 총 3종류가 있기 때문이다.\n
-        Mus caroli / Mus musculus / Mus pahari
-        
-        example:
-        ```python
-        from genet import database as db
-
-        # To get BRCA1 gene sequence information from mouse
-        gene = db.GetGene('BRCA1', species='Mus musculus')
-        ```
-
-        '''
 
         print('Find %s from NCBI nucleotide database' % gene_name)
-        search_string = '%s[Gene] AND %s[Organism] %s' % (gene_name, species, search_option)
+        search_string = '%s[Gene] AND %s[title] AND %s[Organism] %s' % (gene_name, gene_name, species, search_option)
         
         self.handle      = Entrez.esearch(db="nucleotide", term=search_string)
         self.gene_record = Entrez.read(self.handle)
@@ -85,13 +83,27 @@ class GetGene:
 
         return list_transcripts
 
+    def cds(self):
+        '''
+        esearch로 가져온 RefSeq의 ID를 받아서, efetch로 정보를 불러온다.
+        불러온 정보는 seq_record로 저장되고, 그 안에서 각종 정보를 가져올 수 있다.
+        
+        '''
+        def is_cds(feat):      return feat.type == 'CDS'
+
+        self.feat = self.seq_record.features
+        list_transcripts = [f for f in filter(is_cds, self.feat)]
+
+        return list_transcripts
+    
+
     def misc(self):
         '''
         esearch로 가져온 RefSeq의 ID를 받아서, efetch로 정보를 불러온다.
         불러온 정보는 seq_record로 저장되고, 그 안에서 각종 정보를 가져올 수 있다.
         
         '''
-        def is_misc_feat(self, feat): return feat.type == 'misc_feature'
+        def is_misc_feat(feat): return feat.type == 'misc_feature'
 
         self.feat = self.seq_record.features
         list_transcripts = [f for f in filter(is_misc_feat, self.feat)]
@@ -104,7 +116,7 @@ class GetGene:
         불러온 정보는 seq_record로 저장되고, 그 안에서 각종 정보를 가져올 수 있다.
         
         '''
-        def is_source(self, feat):    return feat.type == 'source'
+        def is_source(feat):    return feat.type == 'source'
 
         self.feat = self.seq_record.features
         list_transcripts = [f for f in filter(is_source, self.feat)]
@@ -117,13 +129,13 @@ class GetClinVar:
     NCBI ClinVar에서 record를 찾기위한 function.\n
     기본적으로 biopython의 Entrez module을 사용한다. 
 
-    example:
+    ## example:
     >>> from genet import database as db
     >>> cv_record = db.GetClinVar('VCV000428864')
 
     GetClinVar class에서 seq method로 해당 record sequence를 fetching 할 수 있다.
 
-    example:
+    ## example:
     >>> ref_seq, alt_seq = cv_record.seq()
 
     output:
