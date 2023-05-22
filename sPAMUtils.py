e442d59406a85b6084f4fc863f1852c9596694a7
Goosang Yu <gsyu93@gmail.com>
2023-05-22 10:30:01 +0900

    ✨ make synonymousPAM class

---
+++ sPAMUtils.py
@@ -0,0 +1,24 @@
+
+dict_pam_disrup_rank = {
+    '''
+    GG PAM 에서 mutation이 생겨서 PAM disruption이 일어날 때,
+    prime editing efficiency가 증가하는 순위
+    Reference: Yu et al. Cell 2023
+    '''
+    'AT': 1,
+    'TA': 2,
+    'AG': 3,
+    'TT': 4,
+    'TG': 5,
+    'CT': 6,
+    'TC': 7,
+    'AC': 8,
+    'GT': 9,
+    'CC': 10,
+    'AA': 11,
+    'CA': 12,
+    'GA': 13,
+    'GC': 14,
+    'CG': 15,
+    'GG': 16,
+}
