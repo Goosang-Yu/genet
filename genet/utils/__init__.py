122162353becd3b2a5d08606aaa493b3504b6488
Goosang Yu <gsyu93@gmail.com>
2023-10-27 00:39:19 +0900

    update command for PYPI release

--- genet/utils/__init__.py
+++ genet/utils/__init__.py
@@ -1,7 +1,7 @@
 from .functional import *
 
-# for PyPI release
-# python setup.py bdist_wheel
+# for PyPI release and upload source URL
+# python setup.py sdist bdist_wheel
 # twine upload dist/genet-0.5.2-py3-none-any.whl
 
 version_ = '0.10.0'
