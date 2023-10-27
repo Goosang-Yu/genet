bc7ac929c4ef77d81bb43e0186d3fa8b7ec174bf
Goosang Yu <gsyu93@gmail.com>
2023-10-28 00:30:22 +0900

    ✅ Change release tools setup.py to hatch

--- genet/utils/__init__.py
+++ genet/utils/__init__.py
@@ -1,10 +1,1 @@
 from .functional import *
-
-# for PyPI release and upload source URL
-# python setup.py sdist bdist_wheel
-# twine upload dist/genet-0.5.2-py3-none-any.whl
-
-version_ = '0.10.0'
-
-__version__ = version_
-
