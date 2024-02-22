17a94ad2839d31a74c1fcb5db26626a05a10bb9c
Goosang Yu <gsyu93@gmail.com>
2024-02-22 18:18:05 +0900

    🚧 CasOFFinder_wrapper

--- genet/analysis/OFFinder.py
+++ genet/analysis/OFFinder.py
@@ -1,22 +1,28 @@
+import time, os, sys, subprocess
 import pandas as pd
 from Bio import SeqIO
-import time, os, sys
 from tqdm import tqdm
 
 
 
-class CasOFFinderWrapper:
-    def __init__(self, ):
-        '''Cas-OFFinder를 python 함수로 불러와서 사용할 수 있게 하는 wrapper.
+
+class CasOFFinder:
+    def __init__(self, custom_path:str):
+        """Cas-OFFinder를 python 함수로 불러와서 사용할 수 있게 하는 wrapper.
         사용하는 환경에 Cas-OFFinder가 설치되어 있어야 한다. 
 
         Input으로 받는 것은 Cas-OFFinder의 것을 따른다.
-        '''
 
+        Args:
+            custom_path (str): _description_
+        """        
+
+
+        
 
         pass
 
-    def casoffinder_output_to_dataframe(self,) -> pd.DataFrame:
+    def run(self, pam:str, output:str, use_gpu=True) -> pd.DataFrame:
         '''Cas-OFFinder의 output 파일을 pandas DataFrame 형태로 만들어주는 함수.'''
 
 
