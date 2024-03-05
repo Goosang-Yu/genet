9e640775b680b50babc44d97a8b9ba4aa6104ed5
Goosang Yu <gsyu93@gmail.com>
2024-03-05 22:02:19 +0900

    🚧 make_mismatch function

--- genet/design/functional.py
+++ genet/design/functional.py
@@ -5,6 +5,9 @@ from Bio.Seq import reverse_complement, translate
 from Bio.SeqUtils import gc_fraction
 from genet.design.DesignUtils import dict_pam_disrup_rank, test_score_data
 
+import psutil, math
+from itertools import combinations, product
+
 '''
 TODO
 1. flash를 python code로 구현한 것이 없으므로, 여기서 input은 .fastq 파일만 가능
@@ -542,374 +545,60 @@ class SynonymousPE:
     
 
 
-def mismatch(seq: str, 
-             n: int, 
-             start: int = 0, 
-             end: int = -1, 
-             capital: bool = False,
-             full: bool = False,
-             ):
+def make_mismatch(seq:str, n_mismatch:int, save:str=None) -> pd.DataFrame:
+    """주어진 sequence에 n_mismatch 수 만큼의 mismatch를 만들어주는 함수. 
+
+    Args:
+        seq (str): mismatch를 만들고자 하는 sequence 정보 (DNA 기준, 추후 RNA 추가해주면 좋을듯?)
+        n_mismatch (int): mismatch를 만드는 수
+        save (str): 결과를 저장할 파일 경로. Defaults to None.
+
+    Raises:
+        ValueError: n_mismatch가 sequence의 길이보다 값이 큰 경우 에러 발생
+
+    Returns:
+        None
+    """
     
-    '''
-    seq  : mismatch를 만들고자 하는 sequence 정보 (DNA 기준, 추후 RNA 추가해주면 좋을듯?)
-    n    : mismatch를 만드는 수
-    start: target에서 mismatch를 도입할 시작점
-    end  : target에서 mismatch를 도입할 종료점
-    capital: mismatched nucleotide 표기를 대문자로 할 것인지, True이면 대문자로 표시됨
-    full: 모든 mismatched position, WT, Alt, original seq 등 자세한 내용을 DataFrame으로 받을지.
-    '''
-    
-    from itertools import combinations, product
-    
-    '''
-    아직 미완성!!!!
-    '''
-    
+    if n_mismatch > len(seq): raise ValueError("n_mismatch should be less than or equal to the length of the given sequence")
+
     seq = seq.upper()
-    target_seq = seq[start:end]
-    
     input_len = len(seq)
     list_seq = list(seq)
-    dic = {}
-    loc = list(combinations(range(input_len), n))
-    nucleo_dic = {"A": ["T","G","C"], 
-                  "T": ["A","G","C"], 
-                  "G": ["A","T","C"], 
-                  "C": ["A","T","G"]}
+
+    nucleo_dic = {"A": ["T", "G", "C"], 
+                  "T": ["A", "G", "C"], 
+                  "G": ["A", "T", "C"], 
+                  "C": ["A", "T", "G"]}
     
-    for i in loc:
-        b = list_seq.copy()
-        for k in range(len(i)):
-            b[i[k]] = nucleo_dic[b[i[k]]]
-        lst = list(product(*b))
-        for i in lst:
-            dic [''.join(i)] = input
-            
-    return dic
+    list_pos_combi = combinations(range(input_len), n_mismatch)
+    total_combi = math.comb(n, r) * (3 ** n_mismatch)
+
+    mem = round(psutil.virtual_memory().used/1000000000, 2)
+    print(f"사용 중인 메모리 - After combinations: {mem} GB")
+
+    cnt = 0
+
+    with open(save, 'w') as f:
+        for pos in list_pos_combi:
+
+            cnt += 1
+
+            list_seq_temp = list_seq.copy()
+
+            for i in range(len(pos)):
+                list_seq_temp[pos[i]] = nucleo_dic[list_seq_temp[pos[i]]]
+
+            list_mm_combi = product(*list_seq_temp)
+
+            for mm_seq in list_mm_combi:
+                new_seq = ''.join(mm_seq)
+                diff_positions = [(pos[i], list_seq[pos[i]], new_seq[pos[i]]) for i in range(len(pos)) if list_seq[pos[i]] != new_seq[pos[i]]]
+                diff_str = ', '.join([f"Position: {pos}, Original: {orig}, New: {new}" for pos, orig, new in diff_positions])
+                f.write(f"Original Sequence: {seq}, New Sequence: {new_seq}, Differences: {diff_str}\n")
+
+            if cnt % 10000 == 0: 
+                mem = round(psutil.virtual_memory().used/1000000000, 2)
+                print(f"사용 중인 메모리 - {cnt} combinations: {mem} GB")
+
     
-
-class pegRNA:
-    '''
-    Dev-ing...
-    ToDo: RT-PBS combination dict -> DataFrame transformation
-    PBS / RTT separated information must included.
-    '''
-    def __init__(self, wt_seq, ed_seq, edit_type, edit_len,
-                pbs_min:int=6, pbs_max:int=17, rtt_max=40,
-                pe_system='PE2max'):
-        
-        self.sWTSeq = wt_seq
-        self.sEditedSeq = ed_seq
-        self.sAltKey = edit_type + str(edit_len)
-        self.sAltType = edit_type
-        self.nAltLen = edit_len
-
-        self.nAltIndex = 60
-        self.pbs_range = [pbs_min, pbs_max]
-        self.rtt_max   = rtt_max
-        self.pe_system = pe_system
-
-        self.sGuideKey = ''
-        self.sChrID = ''
-        self.sStrand = ''
-        self.nGenomicPos = 0
-        self.nEditIndex = 0
-        self.nPBSLen = 0
-        self.nRTTLen = 0
-        self.sPBSSeq = ''
-        self.sRTSeq = ''
-        self.sPegRNASeq = ''
-        self.list_sSeqs = []
-        self.type_sub = 0
-        self.type_ins = 0
-        self.type_del = 0
-        self.dict_sSeqs = {}
-        self.dict_sCombos = {}
-        self.dict_sOutput = {}
-
-        if   self.sAltType.startswith('sub'): self.type_sub = 1
-        elif self.sAltType.startswith('del'): self.type_del = 1
-        elif self.sAltType.startswith('ins'): self.type_ins = 1
-
-        
-        self.get_sAltNotation()
-        self.get_all_RT_PBS(nMinPBS=self.pbs_range[0]-1, nMaxPBS=self.pbs_range[1], nMaxRT=self.rtt_max, pe_system=pe_system)
-        self.make_rt_pbs_combinations()
-
-        self.df_out = pd.DataFrame(self.dict_sCombos)
-
-    # def End: get_input
-
-
-    def show_output(self): return self.df_out
-
-    def get_sAltNotation(self):
-        if self.sAltType == 'sub':
-            self.sAltNotation = '%s>%s' % (
-                self.sWTSeq[self.nAltIndex:self.nAltIndex + self.nAltLen], self.sEditedSeq[self.nAltIndex:self.nAltIndex + self.nAltLen])
-
-        elif self.sAltType == 'del':
-            self.sAltNotation = '%s>%s' % (
-                self.sWTSeq[self.nAltIndex:self.nAltIndex + 1 + self.nAltLen], self.sEditedSeq[self.nAltIndex])
-
-        else:
-            self.sAltNotation = '%s>%s' % (
-                self.sWTSeq[self.nAltIndex], self.sEditedSeq[self.nAltIndex:self.nAltIndex + self.nAltLen + 1])
-
-    # def END: get_sAltNotation
-
-    def get_all_RT_PBS(self, 
-                    nMinPBS = 0,
-                    nMaxPBS = 17,
-                    nMaxRT = 40,
-                    nSetPBSLen = 0,
-                    nSetRTLen = 0,
-                    pe_system = 'PE2'
-                    ):
-        """
-        nMinPBS: If you set specific number, lower than MinPBS will be not generated. Default=0
-        nMaxPBS: If you set specific number, higher than MinPBS will be not generated. Default=17
-        nMaxRT = : If you set specific number, higher than MinPBS will be not generated. Default=40
-        nSetPBSLen = 0  # Fix PBS Len: Set if >0
-        nSetRTLen = 0  # Fix RT  Len: Set if >0
-        PAM: 4-nt sequence
-        """
-
-        nMaxEditPosWin = nMaxRT + 3  # Distance between PAM and mutation
-
-        dict_sWinSize = {'sub': {1: [nMaxRT - 1 - 3, 6], 2: [nMaxRT - 2 - 3, 6], 3: [nMaxRT - 3 - 3, 6]},
-                        'ins': {1: [nMaxRT - 2 - 3, 6], 2: [nMaxRT - 3 - 3, 6], 3: [nMaxRT - 4 - 3, 6]},
-                        'del': {1: [nMaxRT - 1 - 3, 6], 2: [nMaxRT - 1 - 3, 6], 3: [nMaxRT - 1 - 3, 6]}}
-
-        
-        if 'NRCH' in pe_system: # for NRCH-PE PAM
-            dict_sRE = {'+': '[ACGT][ACGT]G[ACGT]|[ACGT][CG]A[ACGT]|[ACGT][AG]CC|[ATCG]ATG', 
-                        '-': '[ACGT]C[ACGT][ACGT]|[ACGT]T[CG][ACGT]|G[GT]T[ACGT]|ATT[ACGT]|CAT[ACGT]|GGC[ACGT]|GTA[ACGT]'} 
-        else:
-            dict_sRE = {'+': '[ACGT]GG[ACGT]', '-': '[ACGT]CC[ACGT]'} # for Original-PE PAM
-
-        for sStrand in ['+', '-']:
-
-            sRE = dict_sRE[sStrand]
-            for sReIndex in regex.finditer(sRE, self.sWTSeq, overlapped=True):
-
-                if sStrand == '+':
-                    nIndexStart = sReIndex.start()
-                    nIndexEnd = sReIndex.end() - 1
-                    sPAMSeq = self.sWTSeq[nIndexStart:nIndexEnd]
-                    sGuideSeq = self.sWTSeq[nIndexStart - 20:nIndexEnd]
-                else:
-                    nIndexStart = sReIndex.start() + 1
-                    nIndexEnd = sReIndex.end()
-                    sPAMSeq = reverse_complement(self.sWTSeq[nIndexStart:nIndexEnd])
-                    sGuideSeq = reverse_complement(self.sWTSeq[nIndexStart:nIndexEnd + 20])
-
-                nAltPosWin = set_alt_position_window(sStrand, self.sAltKey, self.nAltIndex, nIndexStart, nIndexEnd,
-                                                    self.nAltLen)
-
-                ## AltPosWin Filter ##
-                if nAltPosWin <= 0:             continue
-                if nAltPosWin > nMaxEditPosWin: continue
-
-                nPAM_Nick = set_PAM_nicking_pos(sStrand, self.sAltType, self.nAltLen, self.nAltIndex, nIndexStart, nIndexEnd)
-
-                if not check_PAM_window(dict_sWinSize, sStrand, nIndexStart, nIndexEnd, self.sAltType, self.nAltLen,
-                                        self.nAltIndex): continue
-
-                sPAMKey = '%s,%s,%s,%s,%s,%s,%s' % (
-                    self.sAltKey, self.sAltNotation, sStrand, nPAM_Nick, nAltPosWin, sPAMSeq, sGuideSeq)
-
-                dict_sRT, dict_sPBS = self.determine_PBS_RT_seq(sStrand, nMinPBS, nMaxPBS, nMaxRT, nSetPBSLen,
-                                                        nSetRTLen, nPAM_Nick, nAltPosWin, self.sEditedSeq)
-
-                nCnt1, nCnt2 = len(dict_sRT), len(dict_sPBS)
-                if nCnt1 == 0: continue
-                if nCnt2 == 0: continue
-                
-                if sPAMKey not in self.dict_sSeqs:
-                    self.dict_sSeqs[sPAMKey] = ''
-                self.dict_sSeqs[sPAMKey] = [dict_sRT, dict_sPBS]
-
-            # loop END: sReIndex
-        # loop END: sStrand
-
-
-    # def END: get_all_RT_PBS
-
-    def determine_PBS_RT_seq(self, sStrand, nMinPBS, nMaxPBS, nMaxRT, nSetPBSLen, nSetRTLen, nPAM_Nick,
-                            nAltPosWin, sForTempSeq):
-        dict_sPBS = {}
-        dict_sRT = {}
-
-        list_nPBSLen = [nNo + 1 for nNo in range(nMinPBS, nMaxPBS)]
-        for nPBSLen in list_nPBSLen:
-
-            ## Set PBS Length ##
-            if nSetPBSLen:
-                if nPBSLen != nSetPBSLen: continue
-
-            if sStrand == '+':
-                nPBSStart = nPAM_Nick - nPBSLen  # 5' -> PamNick
-                nPBSEnd = nPAM_Nick
-                sPBSSeq = sForTempSeq[nPBSStart:nPBSEnd] # sForTempSeq = self.EditedSeq
-
-            else:
-                if self.sAltKey.startswith('sub'):
-                    nPBSStart = nPAM_Nick
-                elif self.sAltKey.startswith('ins'):
-                    nPBSStart = nPAM_Nick + self.nAltLen
-                elif self.sAltKey.startswith('del'):
-                    nPBSStart = nPAM_Nick - self.nAltLen
-
-                sPBSSeq = reverse_complement(sForTempSeq[nPBSStart:nPBSStart + nPBSLen]) # sForTempSeq = self.EditedSeq
-
-            # if END: sStrand
-
-            sKey = len(sPBSSeq)
-            if sKey not in dict_sPBS:
-                dict_sPBS[sKey] = ''
-            dict_sPBS[sKey] = sPBSSeq
-        # loop END: nPBSLen
-
-        if sStrand == '+':
-            if self.sAltKey.startswith('sub'):
-                list_nRTPos = [nNo + 1 for nNo in range(self.nAltIndex + self.nAltLen, (nPAM_Nick + nMaxRT))] # OK
-            elif self.sAltKey.startswith('ins'):
-                list_nRTPos = [nNo + 1 for nNo in range(self.nAltIndex + self.nAltLen, (nPAM_Nick + nMaxRT))] # OK
-            else:
-                list_nRTPos = [nNo + 1 for nNo in range(self.nAltIndex, (nPAM_Nick + nMaxRT))] ## 수정! ## del2 RHA 3 del1 RHA2
-        else:
-            if self.sAltKey.startswith('sub'):
-                list_nRTPos = [nNo for nNo in range(nPAM_Nick - 1 - nMaxRT, self.nAltIndex)] ## 수정! ## sub1 sub 3 RHA 0
-            else:
-                list_nRTPos = [nNo for nNo in range(nPAM_Nick - 3 - nMaxRT, self.nAltIndex + self.nAltLen - 1)] ## 수정! ## ins2 최소가 2까지 ins3 RHA 최소 3 #del2 RHA 2 del1 RHA1
-        for nRTPos in list_nRTPos:
-
-            if sStrand == '+':
-                nRTStart = nPAM_Nick  # PamNick -> 3'
-                nRTEnd = nRTPos
-                sRTSeq = sForTempSeq[nRTStart:nRTEnd]
-
-            else:
-                if self.sAltKey.startswith('sub'):
-                    nRTStart = nRTPos
-                    nRTEnd = nPAM_Nick  # PamNick -> 3'
-                elif self.sAltKey.startswith('ins'):
-                    nRTStart = nRTPos
-                    nRTEnd = nPAM_Nick + self.nAltLen  # PamNick -> 3'
-                elif self.sAltKey.startswith('del'):
-                    nRTStart = nRTPos
-                    nRTEnd = nPAM_Nick - self.nAltLen  # PamNick -> 3'
-
-                sRTSeq = reverse_complement(sForTempSeq[nRTStart:nRTEnd])
-
-                if not sRTSeq: continue
-            # if END: sStrand
-
-            sKey = len(sRTSeq)
-
-            ## Set RT Length ##
-            if nSetRTLen:
-                if sKey != nSetRTLen: continue
-
-            ## Limit Max RT len ##
-            if sKey > nMaxRT: continue
-
-            ## min RT from nick site to mutation ##
-            if self.sAltKey.startswith('sub'):
-                if sStrand == '+':
-                    if sKey < abs(self.nAltIndex - nPAM_Nick): continue
-                else:
-                    if sKey < abs(self.nAltIndex - nPAM_Nick + self.nAltLen - 1): continue ### 
-            else:
-                if sStrand == '-':
-                    if sKey < abs(self.nAltIndex - nPAM_Nick + self.nAltLen - 1): continue
-
-            if self.sAltKey.startswith('ins'):
-                if sKey < nAltPosWin + 1: continue
-
-            if sKey not in dict_sRT:
-                dict_sRT[sKey] = ''
-            dict_sRT[sKey] = sRTSeq
-        # loop END: nRTPos
-
-        return [dict_sRT, dict_sPBS]
-
-
-    # def END: determine_PBS_RT_seq
-
-    def make_rt_pbs_combinations(self):
-        for sPAMKey in self.dict_sSeqs:
-
-            dict_sRT, dict_sPBS = self.dict_sSeqs[sPAMKey]
-
-            list_sRT = [dict_sRT[sKey] for sKey in dict_sRT]
-            list_sPBS = [dict_sPBS[sKey] for sKey in dict_sPBS]
-
-            if sPAMKey not in self.dict_sCombos:
-                self.dict_sCombos[sPAMKey] = ''
-            self.dict_sCombos[sPAMKey] = {'%s,%s' % (sRT, sPBS): {} for sRT in list_sRT for sPBS in list_sPBS}
-        # loop END: sPAMKey
-    # def END: make_rt_pbs_combinations
-
-
-def reverse_complement(sSeq):
-    dict_sBases = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N', 'U': 'U', 'n': '',
-                   '.': '.', '*': '*', 'a': 't', 'c': 'g', 'g': 'c', 't': 'a'}
-    list_sSeq = list(sSeq)  # Turns the sequence in to a gigantic list
-    list_sSeq = [dict_sBases[sBase] for sBase in list_sSeq]
-    return ''.join(list_sSeq)[::-1]
-
-# def END: reverse_complement
-
-def set_alt_position_window(sStrand, sAltKey, nAltIndex, nIndexStart, nIndexEnd, nAltLen):
-    if sStrand == '+':
-
-        if sAltKey.startswith('sub'):
-            return (nAltIndex + 1) - (nIndexStart - 3)
-        else:
-            return (nAltIndex + 1) - (nIndexStart - 3)
-
-    else:
-        if sAltKey.startswith('sub'):
-            return nIndexEnd - nAltIndex + 3 - (nAltLen - 1)
-
-        elif sAltKey.startswith('del'):
-            return nIndexEnd - nAltIndex + 3 - nAltLen
-
-        else:
-            return nIndexEnd - nAltIndex + 3 + nAltLen
-        # if END:
-    # if END:
-
-# def END: set_alt_position_window
-
-
-def set_PAM_nicking_pos(sStrand, sAltType, nAltLen, nAltIndex, nIndexStart, nIndexEnd):
-    if sStrand == '-':
-        nPAM_Nick = nIndexEnd + 3
-    else:
-        nPAM_Nick = nIndexStart - 3
-
-    return nPAM_Nick
-
-# def END: set_PAM_Nicking_Pos
-
-
-def check_PAM_window(dict_sWinSize, sStrand, nIndexStart, nIndexEnd, sAltType, nAltLen, nAltIndex):
-    nUp, nDown = dict_sWinSize[sAltType][nAltLen]
-
-    if sStrand == '+':
-        nPAMCheck_min = nAltIndex - nUp + 1
-        nPAMCheck_max = nAltIndex + nDown + 1
-    else:
-        nPAMCheck_min = nAltIndex - nDown + 1
-        nPAMCheck_max = nAltIndex + nUp + 1
-    # if END:
-
-    if nIndexStart < nPAMCheck_min or nIndexEnd > nPAMCheck_max:
-        return 0
-    else:
-        return 1
-
-# def END: check_PAM_window
